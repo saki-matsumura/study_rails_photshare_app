3cf7a2aa8fa68e791fead1129820cf4aff2cd294
“MatsumuraSaki” <massaki3156@gmail.com>
2023-10-16 15:37:29 +0900

    ログイン画面作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    def new
+    end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      # ログイン成功
+
+    else
+      # ログイン失敗
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
 end
